4e174c21f9f81d1a19cf34ef3fbba2886d8bf481
Ondřej Kozel <ondrakozel@outlook.com>
2021-10-12 21:08:38 +0200

    Přidání repositáře a nové třídy
    Vytvořil jsem hlavní třídu aplikace a hello world.

---
+++ src/main/java/cz/okozel/ristral/Aplikace.java
@@ -0,0 +1,11 @@
+package cz.okozel.ristral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aplikace {
+    public static void main(String[] args) {
+        SpringApplication.run(Aplikace.class, args);
+    }
+}
