cc9be02d531fcab8f441b8e59f91b405e56ed6f4
Ondřej Kozel <ondrakozel@outlook.com>
2021-10-17 20:03:21 +0200

    Úprava Schema

--- src/main/java/cz/okozel/ristral/backend/schema/Schema.java
+++ src/main/java/cz/okozel/ristral/backend/schema/Schema.java
@@ -1,14 +1,13 @@
 package cz.okozel.ristral.backend.schema;
 
 import cz.okozel.ristral.backend.AbstractEntity;
-import cz.okozel.ristral.backend.uzivatele.Uzivatel;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
 
 @Entity
 @Table(name = "schemata")
@@ -22,16 +21,12 @@ public class Schema extends AbstractEntity {
     @NotNull
     private String nazev;
 
-    @OneToMany(mappedBy = "schema", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-    private Set<Uzivatel> uzivatele;
-
     public Schema() {
     }
 
     public Schema(TypSchematu typSchematu, String nazev) {
         this.typSchematu = typSchematu;
         this.nazev = nazev;
-        this.uzivatele = new HashSet<>();
     }
 
     public TypSchematu getTypSchematu() {
@@ -44,10 +39,6 @@ public class Schema extends AbstractEntity {
 
     public void setNazev(String nazev) {
         this.nazev = nazev;
-    }
-
-    public Set<Uzivatel> getUzivatele() {
-        return Collections.unmodifiableSet(uzivatele);
     }
 
     @Override
