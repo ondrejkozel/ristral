dc49b74f9cdfa2bb0800ea13db8e6b85f22e955d
Ondřej Kozel <ondrakozel@outlook.com>
2021-10-17 20:03:47 +0200

    Vytvoření třídy Vozidlo

---
+++ src/main/java/cz/okozel/ristral/backend/vozidla/Vozidlo.java
@@ -0,0 +1,23 @@
+package cz.okozel.ristral.backend.vozidla;
+
+import cz.okozel.ristral.backend.AbstractEntity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "vozidla")
+public class Vozidlo extends AbstractEntity {
+
+    @Size(max = 50)
+    @NotBlank
+    private String nazev;
+
+    @Size(max = 250)
+    @NotNull
+    private String popis;
+
+}
