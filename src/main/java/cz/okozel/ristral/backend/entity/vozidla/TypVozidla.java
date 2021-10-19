86ac8fccdd5fbfd0b0e2a2ba0a232ecc4e18154a
Ondřej Kozel <ondrakozel@outlook.com>
2021-10-19 21:25:43 +0200

    Dokončení třídy Vozidlo
    a vytvoření třídy TypVozidla.

---
+++ src/main/java/cz/okozel/ristral/backend/entity/vozidla/TypVozidla.java
@@ -0,0 +1,26 @@
+package cz.okozel.ristral.backend.entity.vozidla;
+
+import cz.okozel.ristral.backend.entity.AbstractSchemaEntity;
+import cz.okozel.ristral.backend.entity.schema.Schema;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "typy_vozidel")
+public class TypVozidla extends AbstractSchemaEntity {
+
+    private String nazev;
+
+    public TypVozidla() {}
+
+    public TypVozidla(String nazev, Schema schema) {
+        super(schema);
+        this.nazev = nazev;
+    }
+
+    public String getNazev() {
+        return nazev;
+    }
+
+}
--- src/main/java/cz/okozel/ristral/backend/entity/vozidla/Vozidlo.java
+++ src/main/java/cz/okozel/ristral/backend/entity/vozidla/Vozidlo.java
@@ -1,8 +1,11 @@
 package cz.okozel.ristral.backend.entity.vozidla;
 
 import cz.okozel.ristral.backend.entity.AbstractSchemaEntity;
+import cz.okozel.ristral.backend.entity.schema.Schema;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -20,4 +23,40 @@ public class Vozidlo extends AbstractSchemaEntity {
     @NotNull
     private String popis;
 
+    /**
+     * maximální počet pasažérů
+     */
+    @Size(min = 1)
+    private Integer kapacita;
+
+    @ManyToOne()
+    @JoinColumn()
+    @NotNull
+    private TypVozidla typ;
+
+    public Vozidlo() {}
+
+    public Vozidlo(String nazev, String popis, Integer kapacita, TypVozidla typ, Schema schema) {
+        super(schema);
+        this.nazev = nazev;
+        this.popis = popis;
+        this.kapacita = kapacita;
+        this.typ = typ;
+    }
+
+    public String getNazev() {
+        return nazev;
+    }
+
+    public String getPopis() {
+        return popis;
+    }
+
+    public Integer getKapacita() {
+        return kapacita;
+    }
+
+    public TypVozidla getTyp() {
+        return typ;
+    }
 }
