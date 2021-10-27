243741a473ade0c453b26b97e8599261c15dbfc8
Ondřej Kozel <ondrakozel@outlook.com>
2021-10-27 21:56:17 +0200

    Umožnění přístupu k aktivitám podle uživatele

--- src/main/java/cz/okozel/ristral/backend/repository/AktivitaRepository.java
+++ src/main/java/cz/okozel/ristral/backend/repository/AktivitaRepository.java
@@ -1,7 +1,11 @@
 package cz.okozel.ristral.backend.repository;
 
 import cz.okozel.ristral.backend.entity.aktivity.Aktivita;
+import cz.okozel.ristral.backend.entity.uzivatele.Uzivatel;
 import cz.okozel.ristral.backend.repository.generic.GenericSchemaRepository;
 
+import java.util.List;
+
 public interface AktivitaRepository extends GenericSchemaRepository<Aktivita> {
+    List<Aktivita> findAllByAkterEquals(Uzivatel akter);
 }
--- src/main/java/cz/okozel/ristral/backend/service/AktivitaService.java
+++ src/main/java/cz/okozel/ristral/backend/service/AktivitaService.java
@@ -1,15 +1,22 @@
 package cz.okozel.ristral.backend.service;
 
 import cz.okozel.ristral.backend.entity.aktivity.Aktivita;
+import cz.okozel.ristral.backend.entity.uzivatele.Uzivatel;
 import cz.okozel.ristral.backend.repository.AktivitaRepository;
 import cz.okozel.ristral.backend.service.generic.GenericSchemaService;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AktivitaService extends GenericSchemaService<Aktivita, AktivitaRepository> {
 
     public AktivitaService(AktivitaRepository hlavniRepositar) {
         super(hlavniRepositar);
+    }
+
+    public List<Aktivita> findAll(Uzivatel uzivatel) {
+        return hlavniRepositar.findAllByAkterEquals(uzivatel);
     }
 
 }
