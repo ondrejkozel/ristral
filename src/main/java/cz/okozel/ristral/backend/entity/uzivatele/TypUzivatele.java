8db83ff378fdd91c928f1d15c17b93ba84b402f1
Ondřej Kozel <ondrakozel@outlook.com>
2021-10-28 12:07:26 +0200

    TypUzivatele.getTypUzivatele nyní používá HashMap
    Toto by měl být o něco elegantnější přístup. Jeho výhody by se ukázaly
    hlavně při vysokém počtu typů uživatelů.

--- src/main/java/cz/okozel/ristral/backend/entity/uzivatele/TypUzivatele.java
+++ src/main/java/cz/okozel/ristral/backend/entity/uzivatele/TypUzivatele.java
@@ -1,13 +1,15 @@
 package cz.okozel.ristral.backend.entity.uzivatele;
 
+import java.util.HashMap;
+
 public enum TypUzivatele {
     UZIVATEL_ORG("uživatel organizace", UzivatelOrg.class),
     ADMIN_ORG("administrátor organizace", AdminOrg.class),
     OSOBNI_UZIVATEL("uživatel osobního účtu", OsobniUzivatel.class),
     SUPERADMIN_ORG("superadministrátor organizace", SuperadminOrg.class);
 
-    private String nazev;
-    private Class<? extends Uzivatel> trida;
+    private final String nazev;
+    private final Class<? extends Uzivatel> trida;
 
     TypUzivatele(String nazev, Class<? extends Uzivatel> trida) {
         this.nazev = nazev;
@@ -18,13 +20,18 @@ public enum TypUzivatele {
         return nazev;
     }
 
-    private Class<? extends Uzivatel> getTrida() {
-        return trida;
-    }
-
     @Override
     public String toString() {
         return nazev;
+    }
+
+    private static final HashMap<Class<? extends Uzivatel>, TypUzivatele> TYPY_UZIVATELU = new HashMap<>();
+
+    static {
+        for (TypUzivatele value : values()) {
+            TYPY_UZIVATELU.put(value.trida, value);
+        }
+        System.out.println(TYPY_UZIVATELU);
     }
 
     public static TypUzivatele getTypUzivatele(Class<? extends Uzivatel> trida) {
