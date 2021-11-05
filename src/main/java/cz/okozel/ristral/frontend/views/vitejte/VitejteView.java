26d273e9d922e77f203c843e92dec80887876202
Ondřej Kozel <ondrakozel@outlook.com>
2021-11-05 22:57:36 +0100

    Vytvoření hlavní stránky

---
+++ src/main/java/cz/okozel/ristral/frontend/views/vitejte/VitejteView.java
@@ -0,0 +1,31 @@
+package cz.okozel.ristral.frontend.views.vitejte;
+
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.auth.AnonymousAllowed;
+import cz.okozel.ristral.frontend.MainLayout;
+
+@PageTitle("Vítejte")
+@Route(value = "", layout = MainLayout.class)
+@AnonymousAllowed
+public class VitejteView extends VerticalLayout {
+
+    public VitejteView() {
+        Image img = new Image("images/empty-plant.png", "placeholder plant");
+        img.setWidth("200px");
+        add(img);
+
+        add(new H2("This place intentionally left empty"));
+        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+
+        setSizeFull();
+        setJustifyContentMode(JustifyContentMode.CENTER);
+        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+        getStyle().set("text-align", "center");
+    }
+
+}
