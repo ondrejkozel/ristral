74a35c9d94adcf6de6e37bbd8a1d8ef5541d5d99
Ondřej Kozel <ondrakozel@outlook.com>
2021-11-05 22:58:02 +0100

    Úprava hlavní třídy aplikace

--- src/main/java/cz/okozel/ristral/RistralAplikace.java
+++ src/main/java/cz/okozel/ristral/RistralAplikace.java
@@ -1,12 +1,19 @@
 package cz.okozel.ristral;
 
+import com.vaadin.flow.component.dependency.NpmPackage;
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.vaadin.artur.helpers.LaunchUtil;
 
 @SpringBootApplication(exclude = ErrorMvcAutoConfiguration.class)
-public class RistralAplikace {
+@Theme(value = "ristral")
+@NpmPackage(value = "line-awesome", version = "1.3.0")
+public class RistralAplikace  extends SpringBootServletInitializer implements AppShellConfigurator {
     public static void main(String[] args) {
-        SpringApplication.run(RistralAplikace.class, args);
+        LaunchUtil.launchBrowserInDevelopmentMode(SpringApplication.run(RistralAplikace.class, args));
     }
 }
