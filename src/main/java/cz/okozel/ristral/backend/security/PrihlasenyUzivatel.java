685069dd6cf27f1297decc905fbe81111240bfc5
Ondřej Kozel <ondrakozel@outlook.com>
2021-11-05 22:55:29 +0100

    Dodělání zabezpečení

--- src/main/java/cz/okozel/ristral/backend/security/PrihlasenyUzivatel.java
+++ src/main/java/cz/okozel/ristral/backend/security/PrihlasenyUzivatel.java
@@ -1,10 +1,14 @@
 package cz.okozel.ristral.backend.security;
 
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.server.VaadinServletRequest;
 import cz.okozel.ristral.backend.entity.uzivatele.Uzivatel;
 import cz.okozel.ristral.backend.service.UzivatelService;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
@@ -29,6 +33,12 @@ public class PrihlasenyUzivatel {
         UserDetails details = getPrihlasenyUzivatel();
         if (details == null) return Optional.empty();
         return Optional.of(uzivatelService.findByUzivatelskeJmeno(details.getUsername()));
+    }
+
+    public void odhlasSe() {
+        UI.getCurrent().getPage().setLocation(SecurityConfiguration.LOGOUT_URL);
+        SecurityContextLogoutHandler logoutHandler = new SecurityContextLogoutHandler();
+        logoutHandler.logout(VaadinServletRequest.getCurrent().getHttpServletRequest(), null, null);
     }
 
 }
---
+++ src/main/java/cz/okozel/ristral/backend/security/UserDetailsServiceImpl.java
@@ -0,0 +1,41 @@
+package cz.okozel.ristral.backend.security;
+
+import cz.okozel.ristral.backend.entity.uzivatele.Uzivatel;
+import cz.okozel.ristral.backend.service.UzivatelService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UzivatelService uzivatelService;
+
+    public UserDetailsServiceImpl(UzivatelService uzivatelService) {
+        this.uzivatelService = uzivatelService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String uzivatelskeJmeno) throws UsernameNotFoundException {
+        Logger logger = LoggerFactory.getLogger(getClass());
+        Uzivatel uzivatel = uzivatelService.findByUzivatelskeJmeno(uzivatelskeJmeno);
+        if (uzivatel == null) {
+            throw new UsernameNotFoundException("Žádný uživatel s tímto uživatelským jménem: " + uzivatelskeJmeno);
+        } else {
+            return new org.springframework.security.core.userdetails.User(uzivatel.getUzivatelskeJmeno(), uzivatel.getHeslo(),
+                    getAuthorities(uzivatel));
+        }
+    }
+
+    private static List<GrantedAuthority> getAuthorities(Uzivatel uzivatel) {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + uzivatel.getRole().getNazev()));
+    }
+
+}
