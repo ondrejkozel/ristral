3a4ca603aea1a60defbfc0d4bd598b430f4d9eb3
Ondřej Kozel <ondrakozel@outlook.com>
2021-11-06 20:17:46 +0100

    Přesměrování přihlášeného uživatele z VitejteView

--- src/main/java/cz/okozel/ristral/frontend/views/vitejte/VitejteView.java
+++ src/main/java/cz/okozel/ristral/frontend/views/vitejte/VitejteView.java
@@ -4,17 +4,24 @@ import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.BeforeEnterEvent;
+import com.vaadin.flow.router.BeforeEnterObserver;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
+import cz.okozel.ristral.backend.security.PrihlasenyUzivatel;
 import cz.okozel.ristral.frontend.MainLayout;
+import cz.okozel.ristral.frontend.views.prehled.PrehledView;
 
 @PageTitle("Vítejte")
 @Route(value = "", layout = MainLayout.class)
 @AnonymousAllowed
-public class VitejteView extends VerticalLayout {
+public class VitejteView extends VerticalLayout implements BeforeEnterObserver {
 
-    public VitejteView() {
+    private PrihlasenyUzivatel prihlasenyUzivatel;
+
+    public VitejteView(PrihlasenyUzivatel prihlasenyUzivatel) {
+        this.prihlasenyUzivatel = prihlasenyUzivatel;
         Image img = new Image("images/empty-plant.png", "placeholder plant");
         img.setWidth("200px");
         add(img);
@@ -28,4 +35,8 @@ public class VitejteView extends VerticalLayout {
         getStyle().set("text-align", "center");
     }
 
+    @Override
+    public void beforeEnter(BeforeEnterEvent event) {
+        if (prihlasenyUzivatel.get().isPresent()) event.forwardTo(PrehledView.class);
+    }
 }
