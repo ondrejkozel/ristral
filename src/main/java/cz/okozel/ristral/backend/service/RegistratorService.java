c3b8be0dd0ca3945429e3e5e9577aeacf310118d
Ondřej Kozel <ondrakozel@outlook.com>
2021-11-24 19:10:46 +0100

    Vytvoření zvlášní třídy pro registraci uživatelů
    a adekvátní úprava RegisterPresenter.

---
+++ src/main/java/cz/okozel/ristral/backend/service/RegistratorService.java
@@ -0,0 +1,46 @@
+package cz.okozel.ristral.backend.service;
+
+import cz.okozel.ristral.backend.entity.schema.Schema;
+import cz.okozel.ristral.backend.entity.schema.TypSchematu;
+import cz.okozel.ristral.backend.entity.uzivatele.Uzivatel;
+import cz.okozel.ristral.backend.entity.zastavky.RezimObsluhy;
+import cz.okozel.ristral.backend.service.entity.RezimObsluhyService;
+import cz.okozel.ristral.backend.service.entity.SchemaService;
+import cz.okozel.ristral.backend.service.entity.UzivatelService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RegistratorService {
+
+    Logger logger = LoggerFactory.getLogger(RegistratorService.class);
+
+    private final UzivatelService uzivatelService;
+    private final SchemaService schemaService;
+    private final RezimObsluhyService rezimObsluhyService;
+    private final PasswordEncoder passwordEncoder;
+
+    public RegistratorService(UzivatelService uzivatelService, SchemaService schemaService, RezimObsluhyService rezimObsluhyService, PasswordEncoder passwordEncoder) {
+        this.uzivatelService = uzivatelService;
+        this.schemaService = schemaService;
+        this.rezimObsluhyService = rezimObsluhyService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public boolean zaregistruj(Uzivatel uzivatel) {
+        if (uzivatelService.jeTotoUzivateskeJmenoObsazene(uzivatel.getUzivatelskeJmeno())) return false;
+        Schema schema = new Schema(TypSchematu.OSOBNI, uzivatel.getUzivatelskeJmeno());
+        uzivatel.setSchema(schema);
+        uzivatel.setHeslo(passwordEncoder.encode(uzivatel.getHeslo()));
+        schemaService.save(schema);
+        uzivatelService.save(uzivatel);
+        //
+        rezimObsluhyService.save(RezimObsluhy.vytvorVychoziRezimBezZnameni(schema));
+        //
+        logger.info("Byl vytvořen nový uživatel " + uzivatel.getUzivatelskeJmeno());
+        return true;
+    }
+
+}
--- src/main/java/cz/okozel/ristral/frontend/presenters/register/RegisterPresenter.java
+++ src/main/java/cz/okozel/ristral/frontend/presenters/register/RegisterPresenter.java
@@ -4,50 +4,31 @@ import com.vaadin.flow.component.UI;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
-import cz.okozel.ristral.backend.entity.schema.Schema;
-import cz.okozel.ristral.backend.entity.schema.TypSchematu;
 import cz.okozel.ristral.backend.entity.uzivatele.Uzivatel;
-import cz.okozel.ristral.backend.service.SchemaService;
-import cz.okozel.ristral.backend.service.UzivatelService;
+import cz.okozel.ristral.backend.service.RegistratorService;
 import cz.okozel.ristral.frontend.presenters.Presenter;
-import cz.okozel.ristral.frontend.presenters.vitejte.VitejtePresenter;
 import cz.okozel.ristral.frontend.views.login.LoginView;
 import cz.okozel.ristral.frontend.views.register.RegisterView;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @PageTitle("Registrace")
 @Route("register")
 @AnonymousAllowed
 public class RegisterPresenter extends Presenter<RegisterView> {
 
-    Logger logger = LoggerFactory.getLogger(RegisterPresenter.class);
+    private final RegistratorService registratorService;
 
-    private UzivatelService uzivatelService;
-    private SchemaService schemaService;
-    private PasswordEncoder passwordEncoder;
-
-    public RegisterPresenter(UzivatelService uzivatelService, SchemaService schemaService, PasswordEncoder passwordEncoder) {
-        this.uzivatelService = uzivatelService;
-        this.schemaService = schemaService;
-        this.passwordEncoder = passwordEncoder;
+    public RegisterPresenter(RegistratorService registratorService) {
+        this.registratorService = registratorService;
         getContent().addListener(RegisterView.RegisterEvent.UlozEvent.class, this::ulozUzivatele);
     }
 
     private void ulozUzivatele(RegisterView.RegisterEvent.UlozEvent event) {
         Uzivatel uzivatel = event.getUzivatel();
-        if (uzivatelService.jeTotoUzivateskeJmenoObsazene(uzivatel.getUzivatelskeJmeno())) getContent().ukazUzivatelskeJmenoObsazeno(uzivatel.getUzivatelskeJmeno());
-        else {
-            Schema schema = new Schema(TypSchematu.OSOBNI, uzivatel.getUzivatelskeJmeno());
-            uzivatel.setSchema(schema);
-            uzivatel.setHeslo(passwordEncoder.encode(uzivatel.getHeslo()));
-            schemaService.save(schema);
-            uzivatelService.save(uzivatel);
-            logger.info("Byl vytvořen nový uživatel " + uzivatel.getUzivatelskeJmeno());
+        if (registratorService.zaregistruj(uzivatel)) {
             getContent().ukazUspesneZaregistrovano();
             UI.getCurrent().navigate(LoginView.class);
         }
+        else getContent().ukazUzivatelskeJmenoObsazeno(uzivatel.getUzivatelskeJmeno());
     }
 
 }
