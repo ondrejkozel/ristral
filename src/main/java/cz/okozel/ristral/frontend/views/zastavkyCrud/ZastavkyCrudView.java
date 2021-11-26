056d6828af1b49c505ba3399f2b9df614b9130a2
Ondřej Kozel <ondrakozel@outlook.com>
2021-11-26 17:52:25 +0100

    Přidání odkazu ke správě režimů obsluhy
    v ZastavkyCrudView.

--- src/main/java/cz/okozel/ristral/frontend/views/zastavkyCrud/ZastavkyCrudView.java
+++ src/main/java/cz/okozel/ristral/frontend/views/zastavkyCrud/ZastavkyCrudView.java
@@ -1,12 +1,17 @@
 package cz.okozel.ristral.frontend.views.zastavkyCrud;
 
+import com.vaadin.flow.component.UI;
 import cz.okozel.ristral.backend.entity.zastavky.Zastavka;
+import cz.okozel.ristral.frontend.presenters.rezimyObsluhyCrud.RezimyObsluhyCrudPresenter;
 import cz.okozel.ristral.frontend.views.crud.GenericCrudView;
 
 public class ZastavkyCrudView extends GenericCrudView<Zastavka> {
 
     public ZastavkyCrudView() {
         setCrudTexty("Nová zastávka", "Upravit zastávku", "Odstranit zastávku");
+        //
+        pridejHrPodSoubor();
+        pridejMenuItemPodSoubor("Spravovat režimy obsluhy", event -> UI.getCurrent().navigate(RezimyObsluhyCrudPresenter.class), false);
     }
 
     @Override
