a28e718f9fab564ecaa315c65d44c40ccb22ba45
Ondřej Kozel <ondrakozel@outlook.com>
2021-12-02 15:19:47 +0100

    Přidání ikon typům uživatelů

--- src/main/java/cz/okozel/ristral/backend/entity/uzivatele/Role.java
+++ src/main/java/cz/okozel/ristral/backend/entity/uzivatele/Role.java
@@ -1,20 +1,24 @@
 package cz.okozel.ristral.backend.entity.uzivatele;
 
+import cz.okozel.ristral.frontend.Ikona;
+
 import java.util.HashMap;
 
 public enum Role {
-    UZIVATEL_ORG("uživatel organizace", "uzivatelOrg", UzivatelOrg.class),
-    ADMIN_ORG("administrátor organizace", "adminOrg", AdminOrg.class),
-    OSOBNI_UZIVATEL("uživatel osobního účtu", "osobni", OsobniUzivatel.class),
-    SUPERADMIN_ORG("superadministrátor organizace", "superadminOrg", SuperadminOrg.class);
+    UZIVATEL_ORG("řadový uživatel", "uzivatelOrg", Ikona.UZIVATEL, UzivatelOrg.class),
+    ADMIN_ORG("administrátor ", "adminOrg", Ikona.ADMINISTRATOR, AdminOrg.class),
+    OSOBNI_UZIVATEL("uživatel osobního účtu", "osobni", Ikona.UZIVATEL, OsobniUzivatel.class),
+    SUPERADMIN_ORG("superadministrátor", "superadminOrg", Ikona.SUPERADMINISTRATOR, SuperadminOrg.class);
 
     private final String nazev;
     private final String kratkyNazev;
+    private final Ikona ikona;
     private final Class<? extends Uzivatel> trida;
 
-    Role(String nazev, String kratkyNazev, Class<? extends Uzivatel> trida) {
+    Role(String nazev, String kratkyNazev, Ikona ikona, Class<? extends Uzivatel> trida) {
         this.nazev = nazev;
         this.kratkyNazev = kratkyNazev;
+        this.ikona = ikona;
         this.trida = trida;
     }
 
@@ -41,6 +45,10 @@ public enum Role {
 
     public static Role getRole(Class<? extends Uzivatel> trida) {
         return ROLE.get(trida);
+    }
+
+    public Ikona getIkona() {
+        return ikona;
     }
 
 }
