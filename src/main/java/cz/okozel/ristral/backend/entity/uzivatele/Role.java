043edde2f221b000abadfc6e8345d70ee8e32831
Ondřej Kozel <ondrakozel@outlook.com>
2021-12-02 13:49:17 +0100

    Úprava rolí

--- src/main/java/cz/okozel/ristral/backend/entity/uzivatele/Role.java
+++ src/main/java/cz/okozel/ristral/backend/entity/uzivatele/Role.java
@@ -3,16 +3,18 @@ package cz.okozel.ristral.backend.entity.uzivatele;
 import java.util.HashMap;
 
 public enum Role {
-    UZIVATEL_ORG("uživatel organizace", UzivatelOrg.class),
-    ADMIN_ORG("administrátor organizace", AdminOrg.class),
-    OSOBNI_UZIVATEL("uživatel osobního účtu", OsobniUzivatel.class),
-    SUPERADMIN_ORG("superadministrátor organizace", SuperadminOrg.class);
+    UZIVATEL_ORG("uživatel organizace", "uzivatelOrg", UzivatelOrg.class),
+    ADMIN_ORG("administrátor organizace", "adminOrg", AdminOrg.class),
+    OSOBNI_UZIVATEL("uživatel osobního účtu", "osobni", OsobniUzivatel.class),
+    SUPERADMIN_ORG("superadministrátor organizace", "superadminOrg", SuperadminOrg.class);
 
     private final String nazev;
+    private final String kratkyNazev;
     private final Class<? extends Uzivatel> trida;
 
-    Role(String nazev, Class<? extends Uzivatel> trida) {
+    Role(String nazev, String kratkyNazev, Class<? extends Uzivatel> trida) {
         this.nazev = nazev;
+        this.kratkyNazev = kratkyNazev;
         this.trida = trida;
     }
 
@@ -36,6 +38,10 @@ public enum Role {
     public static Role getRole(Class<? extends Uzivatel> trida) {
         for (Role role : values()) if (role.trida.equals(trida)) return role;
         return null;
+    }
+
+    public String getKratkyNazev() {
+        return kratkyNazev;
     }
 
 }
