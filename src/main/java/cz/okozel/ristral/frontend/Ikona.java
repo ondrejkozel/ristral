af2a94706ab745853a91061e174debcfba741a35
Ondřej Kozel <ondrakozel@outlook.com>
2021-12-02 15:19:30 +0100

    Přidání ikon uživatelů

--- src/main/java/cz/okozel/ristral/frontend/Ikona.java
+++ src/main/java/cz/okozel/ristral/frontend/Ikona.java
@@ -6,18 +6,26 @@ public enum Ikona {
     BUS("la la-bus"),
     GRAF("la la-chart-area"),
     ZASTAVKA("las la-map-pin"),
-    INFO("la la-info");
+    INFO("la la-info"),
+    UZIVATELE("las la-users"),
+    UZIVATEL("las la-user"),
+    ADMINISTRATOR("las la-user-edit"),
+    SUPERADMINISTRATOR("las la-user-cog");
 
-    private String lineAwesome;
+    private final String nazevTridy;
 
-    Ikona(String lineAwesome) {
-        this.lineAwesome = lineAwesome;
+    Ikona(String nazevTridy) {
+        this.nazevTridy = nazevTridy;
     }
 
     public Span getSpan() {
         Span span = new Span();
-        span.addClassNames(lineAwesome);
+        span.setClassName(nazevTridy);
         return span;
+    }
+
+    public String getNazevTridy() {
+        return nazevTridy;
     }
 
 }
