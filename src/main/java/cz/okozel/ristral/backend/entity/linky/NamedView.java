3432da026cdd7f1c98e5186a64364cb5a7fd93d6
Ondřej Kozel <ondrakozel@outlook.com>
2022-01-17 18:18:14 +0100

    Odstranění NamedRoute

--- src/main/java/cz/okozel/ristral/backend/entity/linky/NamedView.java
+++ src/main/java/cz/okozel/ristral/backend/entity/linky/NamedView.java
@@ -1,7 +1,39 @@
 package cz.okozel.ristral.backend.entity.linky;
 
-interface NamedView<T> {
-    T data();
-    String name();
-    boolean visible();
+public class NamedView<T> {
+
+    private final T data;
+    private final String name;
+    private final boolean visible;
+
+    public NamedView(T data, String name, boolean visible) {
+        this.data = data;
+        this.name = name;
+        this.visible = visible;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isVisible() {
+        return visible;
+    }
+
+    public NamedView<T> withData(T data) {
+        return new NamedView<>(data, name, visible);
+    }
+
+    public NamedView<T> withName(String name) {
+        return new NamedView<>(data, name, visible);
+    }
+
+    public NamedView<T> withVisible(boolean visible) {
+        return new NamedView<>(data, name, visible);
+    }
+
 }
