23316b712c827bfd507c6101e1a69be625292eec
Ondřej Kozel <ondrakozel@outlook.com>
2022-01-30 16:55:19 +0100

    Vytvoření metody pro sestavení trasy jízdy

--- src/main/java/cz/okozel/ristral/backend/entity/lines/LineRouteCarrier.java
+++ src/main/java/cz/okozel/ristral/backend/entity/lines/LineRouteCarrier.java
@@ -1,8 +1,10 @@
 package cz.okozel.ristral.backend.entity.lines;
 
 import cz.okozel.ristral.backend.entity.AbstractSchemaEntity;
+import cz.okozel.ristral.backend.entity.routes.Link;
 import cz.okozel.ristral.backend.entity.routes.NamedView;
 import cz.okozel.ristral.backend.entity.routes.Route;
+import cz.okozel.ristral.backend.entity.trips.TripRouteLinkData;
 import cz.okozel.ristral.backend.entity.zastavky.Zastavka;
 import org.hibernate.annotations.Cascade;
 import org.hibernate.annotations.CascadeType;
@@ -10,6 +12,7 @@ import org.hibernate.annotations.CascadeType;
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -52,6 +55,16 @@ public class LineRouteCarrier extends AbstractSchemaEntity {
     private Route<Zastavka, LineRouteLinkData> buildRoute() {
         Route.Joiner<Zastavka, LineRouteLinkData> joiner = Route.empty();
         linkCarriers.forEach(linkCarrier -> joiner.join(linkCarrier.getLink()));
+        return joiner.finishOptionally().orElseThrow();
+    }
+
+    public static Route<String, TripRouteLinkData> buildTripRoute(Route<Zastavka, LineRouteLinkData> lineRoute, LocalDateTime timeOfDeparture) {
+        Route.Joiner<String, TripRouteLinkData> joiner = Route.empty();
+        for (Link<Zastavka, LineRouteLinkData> link : lineRoute.links()) {
+            LocalDateTime arrivalToNext = timeOfDeparture.plus(link.edge().getDuration());
+            joiner.join(Link.build(link.from().getNazev(), link.to().getNazev(), new TripRouteLinkData(timeOfDeparture, arrivalToNext)));
+            timeOfDeparture = arrivalToNext;
+        }
         return joiner.finishOptionally().orElseThrow();
     }
 }
