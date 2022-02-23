79b54fe5fe9924b038f4e0bec5ea9fee2156cd06
Ondřej Kozel <ondrakozel@outlook.com>
2022-02-23 20:48:38 +0100

    Vytvoření LinkyCrud

--- src/main/java/cz/okozel/ristral/frontend/presenters/linkyCrud/LinkyCrudPresenter.java
+++ src/main/java/cz/okozel/ristral/frontend/presenters/linkyCrud/LinkyCrudPresenter.java
@@ -1,14 +1,24 @@
 package cz.okozel.ristral.frontend.presenters.linkyCrud;
 
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.crud.BinderCrudEditor;
 import com.vaadin.flow.component.crud.CrudEditor;
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.textfield.TextArea;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.binder.BeanValidationBinder;
 import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import cz.okozel.ristral.backend.entity.lines.Line;
+import cz.okozel.ristral.backend.entity.schema.Schema;
+import cz.okozel.ristral.backend.entity.vozidla.TypVozidla;
 import cz.okozel.ristral.backend.security.PrihlasenyUzivatel;
 import cz.okozel.ristral.backend.service.entity.LineService;
+import cz.okozel.ristral.backend.service.entity.TypVozidlaService;
 import cz.okozel.ristral.frontend.MainLayout;
 import cz.okozel.ristral.frontend.presenters.crud.GenericCrudPresenter;
 import cz.okozel.ristral.frontend.views.linkyCrud.LinkyCrudView;
@@ -20,13 +30,59 @@ import javax.annotation.security.PermitAll;
 @PermitAll
 public class LinkyCrudPresenter extends GenericCrudPresenter<Line, LinkyCrudView> {
 
-    public LinkyCrudPresenter(LineService lineService, PrihlasenyUzivatel prihlasenyUzivatel) {
+    private final TypVozidlaService typVozidlaService;
+    private final Schema aktSchema;
+
+    public LinkyCrudPresenter(LineService lineService, PrihlasenyUzivatel prihlasenyUzivatel, TypVozidlaService typVozidlaService) {
         super(Line.class, new LinkyCrudDataProvider(lineService, Line.class, prihlasenyUzivatel.getPrihlasenyUzivatel().orElseThrow().getSchema()), prihlasenyUzivatel);
+        aktSchema = prihlasenyUzivatel.getPrihlasenyUzivatel().get().getSchema();
+        this.typVozidlaService = typVozidlaService;
+        //
+        getContent().getCrud().addNewListener(event -> editRoutesButtonEnabled(false));
+        //
+        getContent().getCrud().addEditListener(event -> editRoutesButtonEnabled(true));
+        //
+        fillComboBox();
     }
+
+    @SuppressWarnings("FieldCanBeLocal")
+    private TextField label;
+    @SuppressWarnings("FieldCanBeLocal")
+    private TextArea description;
+    private ComboBox<TypVozidla> prefVehicleType;
+
+    private Button editRoutesButton;
 
     @Override
     protected CrudEditor<Line> createEditor() {
+        label = new TextField("Číslo");
+        label.setRequired(true);
+        //
+        prefVehicleType = new ComboBox<>("Preferovaný typ vozidla");
+        //
+        description = new TextArea("Popis");
+        //
+        HorizontalLayout buttonLayout = new HorizontalLayout(buildEditRoutesButton());
+        //
+        FormLayout formLayout = new FormLayout(label, prefVehicleType, description);
+        formLayout.add(buttonLayout);
+        formLayout.setColspan(description, 2);
+        formLayout.setColspan(buttonLayout, 2);
         Binder<Line> binder = new BeanValidationBinder<>(Line.class);
-        return new BinderCrudEditor<>(binder);
+        binder.bindInstanceFields(this);
+        return new BinderCrudEditor<>(binder, formLayout);
+    }
+
+    private void editRoutesButtonEnabled(boolean enabled) {
+        editRoutesButton.setEnabled(enabled);
+    }
+
+    private Button buildEditRoutesButton() {
+        editRoutesButton = new Button("Správa tras", VaadinIcon.ROAD.create());
+        return editRoutesButton;
+    }
+
+    private void fillComboBox() {
+        prefVehicleType.setItems(typVozidlaService.findAll(aktSchema));
     }
 }
