98eb522b65a56b8ac653d24a0845a873bb99ea2c
Ondřej Kozel <ondrakozel@outlook.com>
2022-02-23 18:31:10 +0100

    Nastavení sloupců

--- src/main/java/cz/okozel/ristral/frontend/views/linkyCrud/LinkyCrudView.java
+++ src/main/java/cz/okozel/ristral/frontend/views/linkyCrud/LinkyCrudView.java
@@ -1,7 +1,11 @@
 package cz.okozel.ristral.frontend.views.linkyCrud;
 
+import com.vaadin.flow.component.grid.Grid;
 import cz.okozel.ristral.backend.entity.lines.Line;
 import cz.okozel.ristral.frontend.views.crud.GenericCrudView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LinkyCrudView extends GenericCrudView<Line> {
 
@@ -11,9 +15,14 @@ public class LinkyCrudView extends GenericCrudView<Line> {
 
     @Override
     public void postInicialization() {
-//        deleteAllColumnsExcept("nazev", "popis");
-//        renameColumn("nazev", "Název");
-//        setColumnExpandRatio("popis", 2);
+        deleteAllColumnsExcept("label", "prefVehicleType", "description");
+        renameColumn("label", "Číslo");
+        renameColumn("prefVehicleType", "Preferovaný typ vozidla");
+        renameColumn("description", "Popis");
+        setColumnExpandRatio("description", 2);
+        List<Grid.Column<Line>> columns = new ArrayList<>(getColumns());
+        columns.add(2, columns.remove(0));
+        setColumnOrder(columns);
     }
 
 }
